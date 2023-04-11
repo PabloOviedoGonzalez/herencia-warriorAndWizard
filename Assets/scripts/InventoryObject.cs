7a08088e96ef63ce3c2fa3c779c9d216170146d7
PabloOviedoGonzalez <pablooviedogonzalez@gmail.com>
2023-04-11 09:53:14 +0200

    final inventory herencia

--- Assets/scripts/InventoryObject.cs
+++ Assets/scripts/InventoryObject.cs
@@ -2,8 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class InventoryObject 
+public class InventoryObject : Personaje
 {
+
+    //profe, para q me deje acceder a "health" he puesto q el inventory herede de personaje para tener acceso 
+    //a todo sin bajarle el nivel de proteccion para no hacerlo publico. No se si vale, si no seria hacer el 
+    //"health" publico pero eso es como chungo.
     public string name;
     public float valueToAdd;
 
@@ -20,13 +24,13 @@ public class InventoryObject
 
 
 
-    //public void Use(Warrior w)
-    //{
-    //    Debug.Log(name + w.health + 5);
-    //}
+    public void Use(Warrior w)
+    {
+        Debug.Log(name + health + 5);
+    }
 
-    //public void Use(Wizard wi)
-    //{
-    //    Debug.Log(name + wi.health + 5);
-    //}
+    public void Use(Wizard wi)
+    {
+        Debug.Log(name + health + 5);
+    }
 }
